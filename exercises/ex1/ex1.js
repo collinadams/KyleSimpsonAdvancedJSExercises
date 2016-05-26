ffece3008155b6606eba087ded423c11817c9ec9
Collin Adams <collinadams04@gmail.com>
2016-05-25 18:13:41 -0700

    Avoid global variables with IIFE

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -1,77 +1,79 @@
-A();
+(function(){
+	A();
 
-function C() {
-	console.log("OOPS!");
-}
+	function C() {
+		console.log("OOPS!");
+	}
 
-function E(f) {
-	console.log("E");
-	f();
-	var f = F;
-}
+	function E(f) {
+		console.log("E");
+		f();
+		var f = F;
+	}
 
-var A = function() {
-	console.log("A");
-	B();
-};
-
-var C;
-
-function G() {
-	console.log("G");
-	H();
-
-	var H = function() {
-		console.log("H");
-		I();
+	function A() {
+		console.log("A");
+		B();
 	};
-}
 
-var D = d;
+	var C;
 
-function d() {
-	console.log("D");
-	E();
-}
+	function G() {
+		console.log("G");
+		H();
 
-function I() {
-	console.log("I");
-	J();
-	J();
-}
-
-B = function() {
-	console.log("B");
-	C();
-};
-
-var F = function() {
-	console.log("F");
-	G();
-};
-
-var rest = "KLMNOPQRSTUVWXYZ".split("");
-for (var i=0; i<rest.length; i++) {
-	(function(i){
-		// define the current function
-		window[rest[i]] = function() {
-			console.log(rest[i]);
-			if (i < (rest.length-1)) {
-				// TODO: call the next function
-			}
+		var H = function() {
+			console.log("H");
+			I();
 		};
-	})(i);
-}
+	}
 
-var J = function() {
-	J = function() {
-		console.log("J");
-		K();
+	var D = d;
+
+	function d() {
+		console.log("D");
+		E();
+	}
+
+	function I() {
+		console.log("I");
+		J();
+		J();
+	}
+
+	B = function() {
+		console.log("B");
+		C();
 	};
-};
 
-C = function() {
-	console.log("C");
-	D();
-};
+	var F = function() {
+		console.log("F");
+		G();
+	};
+
+	var rest = "KLMNOPQRSTUVWXYZ".split("");
+	for (var i=0; i<rest.length; i++) {
+		(function(i){
+			// define the current function
+			window[rest[i]] = function() {
+				console.log(rest[i]);
+				if (i < (rest.length-1)) {
+					// TODO: call the next function
+				}
+			};
+		})(i);
+	}
+
+	var J = function() {
+		J = function() {
+			console.log("J");
+			K();
+		};
+	};
+
+	C = function() {
+		console.log("C");
+		D();
+	};
+})();
 
