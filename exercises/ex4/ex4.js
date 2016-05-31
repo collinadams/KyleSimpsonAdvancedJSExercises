345199d40fd19151df30e5cd5d0c274e93987993
Collin Adams <collinadams04@gmail.com>
2016-05-31 11:15:02 -0700

    Use prototypal patter for parent widget and child button

--- exercises/ex4/ex4.js
+++ exercises/ex4/ex4.js
@@ -13,29 +13,31 @@ Widget.prototype.render = function($where){
 	}
 };
 
-function Button(/* ... */) {
-/*
-	...
+function Button(width, height, label) {
+	Widget.call(this, width, height);
+	this.label = label;
+	
 	this.$elem = $("<button>").text(this.label);
-*/
+
 }
 
-/*
-Button -> render = function($where) {
-	// call the parent render()
-	// add a click handler -> onClick
-}
+Button.prototype = Object.create(Widget.prototype);
+Button.prototype.constructer = Button;
 
-Button -> onClick = function(evt) {
-	console.log("...");
-}
+Button.prototype.render = function($where) {
+	Widget.prototype.render.call(this, $where); 
+	this.$elem.bind('click', this.onClick.bind(this));
+};
+
+Button.prototype.onClick = function(evt) {
+	console.log("Button " + this.label + " clicked!");
+};
 
 $(document).ready(function(){
 	var $body = $(document.body);
-	var btn1 = ...;
-	var btn2 = ...;
+	var btn1 = new Button(100, 50, 'btn1');
+	var btn2 = new Button(200, 100, 'btn2');
 
 	btn1.render($body);
 	btn2.render($body);
 });
-*/
